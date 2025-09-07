a22cf8e123bee92e884fc5a5f6ae3d024dc6f920
VisionXSX <sera.getman@gmail.com>
2025-09-07 17:04:23 +0200

    Dz 6

---
+++ Dz 6.3.py
@@ -0,0 +1,9 @@
+number = int(input("Введіть ціле число: "))
+
+while number > 9:
+    product = 1
+    for digit in str(number):
+        product = product * int(digit)
+    number = product
+
+print(number)
