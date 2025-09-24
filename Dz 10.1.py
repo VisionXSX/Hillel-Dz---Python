661d5a5239700b34a50fefc54c58a99cb6cadb69
VisionXSX <sera.getman@gmail.com>
2025-09-24 20:35:15 +0200

    Dz 10

---
+++ Dz 10.1.py
@@ -0,0 +1,15 @@
+from inspect import isgenerator
+
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    current = begin
+    for _ in range(end):
+        yield current
+        current = func(current)
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
